11aba7f56a3cf746bc9236f138844fef90955e38
Dev093235 <dev431140@gmail.com>
2025-06-03 13:18:39 +0530

    Update reply.js

--- commands/reply.js
+++ commands/reply.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+  execute: (api, message, args) => {
+    if (args.length === 0) {
+      api.sendMessage("Usage: !reply <message>", message.threadID);
+      return;
+    }
+    const replyMsg = args.join(" ");
+    api.sendMessage(replyMsg, message.threadID);
+  }
+};
